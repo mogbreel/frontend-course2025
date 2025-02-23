ebbe23cb79c0fcba035995b1cfc2343ef4f736a3
unknown <mohammedxp2020@gmail.com>
2025-02-23 15:36:57 +0200

    Lecture13 javascript intro...

---
+++ Lecture13/script.js
@@ -0,0 +1,37 @@
+// Print Output
+// document.getElementById("myid").innerHTML = "Hello World!";
+// document.getElementById("myid").innerText =
+//     "Hello JavaScript!";
+// document.write("HTml - Css - Js");
+// window.alert("Welcome From Window");
+// console.log("Welcome From Console log");
+// window.print()
+
+// function Hello() {
+//     window.alert("Hello From Function");
+// }
+
+// Sigle Line Comments
+/* 
+
+- Multi
+- Line 
+- Comments
+*/
+
+// Variables 
+// Const - Var - Let
+const myCarsoulImg = [1, 2, 3, 4, 5];
+
+var first_name = "Mohamed"
+
+let last_name = "Ali"
+
+console.log(first_name);
+console.log(myCarsoulImg);
+myCarsoulImg = [6, 7];
+last_name = "Ahamed";
+console.log(last_name)
+console.log(myCarsoulImg)
+
+
