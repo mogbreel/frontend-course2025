ba2bfd621b5eb813be09621a6a43d3f20926876e
unknown <mohammedxp2020@gmail.com>
2025-02-27 23:00:23 +0200

    JS lecture14 are added...

---
+++ Lecture14/script.js
@@ -0,0 +1,61 @@
+/*======= Script File ========
+==============================*/
+// Arithmetic Operator [+, -, *, /, %, **, --, ++]
+
+// let x = 5;
+// let y = 6;
+// // Addition
+// let z = x - y;
+// // Subtraction
+// let l = x - y;
+// // Divition
+// let d = x / y;
+// // Muptilication 
+// let r = x * 6;
+
+// let f = 7;
+// let k = 2;
+// console.log(f % k);
+
+// // let mm = document.getElementById("myid").innerHTML = r;
+
+// let fk = 5;
+
+// let c = fk ** 2;
+// console.log(fk);
+// console.log(c)
+// let psd = --c;
+// console.log(psd);
+
+// Data Types
+
+let name = 'Ali Galilo';
+console.log(name);
+console.log(typeof (name));
+
+let age = 30;
+let price = 9.9
+let discount = -.5;
+console.log(typeof (price))
+
+let vlaueX = 11 + 25 + "BMW";
+console.log(vlaueX)
+
+var IsPresent = true;
+console.log(typeof (IsPresent))
+
+console.log(5 > 22);
+console.log(5 < 22);
+
+let cars = ["BMW", "Firrari", "Lambourghini"]
+
+console.log(cars)
+console.log(typeof (cars))
+
+let dict = { car: "they have wheels", speed: "propertie for the car" }
+console.log(dict)
+console.log(typeof(dict))
+
+let r = BigInt("46545646465456464546545646");
+console.log(typeof(r))
+
